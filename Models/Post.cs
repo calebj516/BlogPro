25961b4ffbed8ea516f092f4154a6e0f538aa353
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-05-16 22:21:00 -0500

    The Post Model
    The model for the Post object. This model is responsible for handling the information that makes up each blogpost in the application.

---
+++ Models/Post.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheBlogProject.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public int BlogId { get; set; } // foreign key (primary key in Blog class)
+        public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(75, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long.", MinimumLength = 2)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(200, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long.", MinimumLength = 2)]
+        public string Abstract { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Created Date")]
+        public DateTime Created { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated Date")]
+        public DateTime? Updated { get; set; } // nullable, allowing the absence of a value
+
+        public bool IsReady { get; set; } // is ready to be viewed?
+
+        public string Slug { get; set; } // derived programatically from the title that the user enters
+
+        public byte[] ImageData { get; set; }
+        public string ContentType { get; set; }
+
+        [NotMapped]
+        public IFormFile Image { get; set; }
+
+    }
+}
