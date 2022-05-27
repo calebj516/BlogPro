e27d7693a02548a5e719f66890827aa5f9c803b8
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-05-26 21:19:49 -0500

    Identity Pages
    Scaffolded Identity pages

--- Program.cs
+++ Program.cs
@@ -5,6 +5,13 @@ using TheBlogProject.Data;
 using TheBlogProject.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<BlogUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();;
 
 // Add services to the container.
 // var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
