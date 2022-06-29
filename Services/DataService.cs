e3a365d621ef5eac983dd544946615314e41f604
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-06-28 20:36:53 -0500

    Services - Data Service - Part 1

---
+++ Services/DataService.cs
@@ -0,0 +1,44 @@
+﻿using TheBlogProject.Data;
+
+namespace TheBlogProject.Services
+{
+    public class DataService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public DataService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task ManageDataAsync()
+        {
+            // Task 1: Seed a few Roles into the system
+            await SeedRolesAsync();
+
+            // Task 2: Seed a few users into the system
+            await SeedUsersAsync();
+        }
+
+        private async Task SeedRolesAsync()
+        {
+            // If there are already roles in the system, do nothing
+            if (_dbContext.Roles.Any())
+            {
+                return;
+            }
+
+            // Otherwise, we want to create a few Roles
+
+
+        }
+
+        private async Task SeedUsersAsync()
+        {
+
+        }
+
+
+
+    }
+}
