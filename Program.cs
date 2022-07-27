74cf9e33ac8d00913d0c49d965e8bb8de8016dc2
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-07-26 20:46:38 -0500

    Login & Register Pages
    Added a login and register page to the landing page of the application

--- Program.cs
+++ Program.cs
@@ -98,6 +98,6 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-//app.MapRazorPages();
+app.MapRazorPages();
 
 app.Run();
