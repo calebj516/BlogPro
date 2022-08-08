35b2bf145f9f14383a0c90d0e5cfa44141155255
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-08-07 20:16:06 -0500

    Features - Working With Tags Part 2a - Create JS
    Functionality to add and delete tags on a post created using JS

---
+++ wwwroot/js/Custom.js
@@ -0,0 +1,31 @@
+﻿let index = 0;
+
+function AddTag() {
+    // Get a reference to the TagEntry input element
+    let tagEntry = document.getElementById("TagEntry");
+
+    // Create a new Select Option
+    let newOption = new Option(tagEntry.value, tagEntry.value);
+    document.getElementById("TagList").options[index++] = newOption;
+
+    // Clear out the TagEntry control
+    tagEntry.value = "";
+    return true;
+}
+
+function DeleteTag() {
+    let tagCount = 1;
+    let tagList = document.getElementById("TagList");
+    while (tagCount > 0) {
+        let selectedIndex = tagList.selectedIndex;
+        if (selectedIndex >= 0) {
+            tagList.options[selectedIndex] = null;
+            --tagCount;
+            index--;
+        } else {
+            tagCount = 0;
+            index--;
+        }
+    }
+
+}
