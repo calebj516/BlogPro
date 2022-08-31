ad9396128a39f484eb5da63f43c137aa72c0f57d
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-08-30 23:32:21 -0500

    Features - Comments Part 4a - Moderating

--- Controllers/CommentsController.cs
+++ Controllers/CommentsController.cs
@@ -131,6 +131,44 @@ namespace TheBlogProject.Controllers
             return View(comment);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Moderate(int id, [Bind("Id,Body,ModeratedBody,ModerationType")] Comment comment)
+        {
+            if(id != comment.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var originalComment = await _context.Comments.Include(c => c.Post).FirstOrDefaultAsync(c => c.Id == comment.Id);
+                try
+                {
+                    originalComment.ModeratedBody = comment.ModeratedBody;
+                    originalComment.ModerationType = comment.ModerationType;
+
+                    originalComment.Moderated = DateTime.Now;
+                    originalComment.ModeratorId = _userManager.GetUserId(User);
+
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CommentExists(comment.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Details", "Posts", new { slug = originalComment.Post.Slug }, "commentSection");
+            }
+            return View(comment);
+        }
+
         // GET: Comments/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
